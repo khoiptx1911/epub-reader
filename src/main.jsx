c264d33f5c0deaad16e15dee64ba76b28f7c3d16
khoiptx1911 <phamtranxuankhoi@gmail.com>
2026-05-17 21:37:19 +0700

    update config

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
-import { CLIENT_ID } from './config'
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
